18263ba741115e91e57512af32d605fbc701535d
luk95 <beppe1995@libero.it>
2017-12-14 12:44:42 +0100

    users fixed

--- src/routes/users.php
+++ src/routes/users.php
@@ -6,26 +6,22 @@ header("Access-Control-Allow-Origin: *");
 
 $app = new \Slim\App;
 
-
-
-
-
-// Get all users 
-$app->get('/api/users', function (Request $request, Response $response){
+// Get all users
+$app->get('/api/users/list', function (Request $request, Response $response){
     $sql = "SELECT * FROM users";
     try
     {
         $db = new db();
-        //connect 
+        //connect
         $db = $db->connect();
         $stmt = oci_parse($db, $sql);
-        if (!oci_execute($stmt)) 
+        if (!oci_execute($stmt))
         {
             $e = oci_error($stmt);
             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
         }
-        
-        while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) 
+
+        while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS))
         {
             //var_dump($row);
             $response->getBody()->write( json_encode($row));
@@ -48,15 +44,15 @@ $app->post('/api/login',function (Request $request, Response $response){
     $passswordHash = hash ("sha256" , $request->getParam('PASSWORD'));
 
     $query = "select * from users WHERE EMAIL = '".$request->getParam('EMAIL')."' and PASSWORD = '".$passswordHash."'";
-    
+
     try
     {
         $db = new db();
-        //connect 
+        //connect
         $db = $db->connect();
-        $stmt = oci_parse($db, $query);       
+        $stmt = oci_parse($db, $query);
         //check errors
-        if (!oci_execute($stmt)) 
+        if (!oci_execute($stmt))
         {
             $e = oci_error($stmt);
             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
@@ -97,18 +93,18 @@ $app->post('/api/register',function(Request $request, Response $response){
     $email = new email();
     $valid = $email->isValid($usermail);
     //var_dump($valid);
-    if ($valid) 
+    if ($valid)
     {
         $passswordHash = hash ("sha256" , $request->getParam('PASSWORD'));
         $query = "insert INTO users (username,name,surname,email,password,phone,cap,city,id_group,country,street) VALUES ('".$user."','".$name."','".$surname."','".$usermail."','".$passswordHash."','".$phone."','".$cap."','".$city."',1,'".$country."','".$street."')";
         try
         {
             $db = new db();
-            //connect 
+            //connect
             $db = $db->connect();
             $stmt = oci_parse($db, $query);
             //check errors
-            if (!oci_execute($stmt)) 
+            if (!oci_execute($stmt))
             {
                 $response->getBody()->write("one or more rows are not correct");
                 //$e = oci_error($stmt);
@@ -127,18 +123,18 @@ $app->post('/api/register',function(Request $request, Response $response){
         {
             echo '{"error":{text: '.$e->getMessage().'}';
         }
-    } 
-    else 
+    }
+    else
     {
         $response->getBody()->write("email not correct");
     }
-    
 
 
-    
 
-   
-    
+
+
+
+
 
 
 });
