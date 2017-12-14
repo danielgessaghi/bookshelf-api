986d76111e519f06d8f8cf48c18986f0df4da8ce
luk95 <beppe1995@libero.it>
2017-12-14 14:12:56 +0100

    io

--- src/routes/books.php
+++ src/routes/books.php
@@ -9,11 +9,61 @@ $app = new \Slim\App;
 
 // Get all books paged in 10 books for page
 $app->get('/api/books/list/{page}', function (Request $request, Response $response){
+  $query = "SELECT * FROM items";
+  try
+  {
+      $db = new db();
+      //connect
+      $db = $db->connect();
+      $stmt = oci_parse($db, $query);
+      if (!oci_execute($stmt))
+      {
+          $e = oci_error($stmt);
+          trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+      }
 
+      while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS))
+      {
+          //var_dump($row);
+          $response->getBody()->write( json_encode($row));
+      }
+      $customers = oci_free_statement($stmt);
+      oci_close($db);
+      return $response;
+  }
+  catch(PDOException $e)
+  {
+      echo '{"error":{text: '.$e->getMessage().'}';
+  }
 });
 // get book info
 $app->get('/api/books/detail/{book_id}', function (Request $request, Response $response){
+  $query = "SELECT * FROM items WHERE ISBN = ";
+  try
+  {
+      $db = new db();
+      //connect
+      $db = $db->connect();
+      $stmt = oci_parse($db, $query);
+      if (!oci_execute($stmt))
+      {
+          $e = oci_error($stmt);
+          trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+      }
 
+      while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS))
+      {
+          //var_dump($row);
+          $response->getBody()->write( json_encode($row));
+      }
+      $customers = oci_free_statement($stmt);
+      oci_close($db);
+      return $response;
+  }
+  catch(PDOException $e)
+  {
+      echo '{"error":{text: '.$e->getMessage().'}';
+  }
 });
 // delete a book ADMIN
 $app->post('/api/books/delate/{book_id}', function (Request $request, Response $response){
@@ -21,5 +71,5 @@ $app->post('/api/books/delate/{book_id}', function (Request $request, Response $
 });
 // add a book ADMIN
 $app->post('/api/books/add', function (Request $request, Response $response){
-  
+
 });
