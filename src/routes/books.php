81744a2fc70eb81b26e47d2ad56a443832b9d13b
danielgessaghi <daniel.gessaghi19971@gmail.com>
2017-12-14 14:09:06 +0100

    fixed get and post

--- src/routes/books.php
+++ src/routes/books.php
@@ -1,25 +1,27 @@
 <?php
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 header("Access-Control-Allow-Origin: *");
 
-$app = new \Slim\App;
+$book = new \Slim\App;
 
 
 // Get all books paged in 10 books for page
-$app->get('/api/books/list/{page}', function (Request $request, Response $response){
+$book->get('/api/books/list/{page}', function (Request $request, Response $response){
 
 });
 // get book info
-$app->get('/api/books/detail/{book_id}', function (Request $request, Response $response){
+$book->get('/api/books/detail/{book_id}', function (Request $request, Response $response){
 
 });
 // delete a book ADMIN
-$app->post('/api/books/delate/{book_id}', function (Request $request, Response $response){
+$book->post('/api/books/delate/{book_id}', function (Request $request, Response $response){
 
 });
+
 // add a book ADMIN
-$app->post('/api/books/add', function (Request $request, Response $response){
+$book->post('/api/books/add', function (Request $request, Response $response){
   
 });
