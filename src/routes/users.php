3b9ada95e7a8fdc5db7b5d2f572fb720f4ff516f
danielgessaghi <daniel.gessaghi19971@gmail.com>
2017-12-14 10:47:33 +0100

    sontihg work

--- src/routes/users.php
+++ src/routes/users.php
@@ -2,32 +2,37 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+header("Access-Control-Allow-Origin: *");
+
 $app = new \Slim\App;
+
+
+
+
 
 // Get all users 
 $app->get('/api/users', function (Request $request, Response $response){
-    $sql = "select * FROM user_data";
+    $sql = "SELECT * FROM users";
     try
     {
         $db = new db();
         //connect 
         $db = $db->connect();
-        $stmt = oci_parse($db, 'SELECT * FROM users');
-        $r = oci_execute($stmt);
-        if (!$r) 
+        $stmt = oci_parse($db, $sql);
+        if (!oci_execute($stmt)) 
         {
             $e = oci_error($stmt);
             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
         }
-
+        
         while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) 
         {
-
-            echo json_encode($row);
-            
+            //var_dump($row);
+            $response->getBody()->write( json_encode($row));
         }
         $customers = oci_free_statement($stmt);
         oci_close($db);
+        return $response;
     }
     catch(PDOException $e)
     {
@@ -35,30 +40,40 @@ $app->get('/api/users', function (Request $request, Response $response){
     }
 });
 
+
+
+// login utente
 $app->post('/api/login',function (Request $request, Response $response){
+
     $passswordHash = hash ("sha256" , $request->getParam('PASSWORD'));
+
     $query = "select * from users WHERE EMAIL = '".$request->getParam('EMAIL')."' and PASSWORD = '".$passswordHash."'";
+    
     try
     {
         $db = new db();
         //connect 
         $db = $db->connect();
-        $stmt = oci_parse($db, $query);
-        $r = oci_execute($stmt);
+        $stmt = oci_parse($db, $query);       
         //check errors
-        if (!$r) 
+        if (!oci_execute($stmt)) 
         {
             $e = oci_error($stmt);
             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
         }
         //responce data
-        while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) 
-        {
-                return json_encode(true);
+        $row = null;
+        $row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
+        if ($row !=null) {
+            $respJSON = json_encode($row);
+            $response->getBody()->write("true");
+        } else {
+            $response->getBody()->write("false");
         }
         //close connection
         $customers = oci_free_statement($stmt);
         oci_close($db);
+        return $response;
     }
     catch(PDOException $e)
     {
@@ -68,6 +83,7 @@ $app->post('/api/login',function (Request $request, Response $response){
 });
 
 $app->post('/api/register',function(Request $request, Response $response){
+
     $user = $request->getParam('USERNAME');
     $name = $request->getParam('NAME');
     $surname = $request->getParam('SURNAME');
@@ -77,29 +93,28 @@ $app->post('/api/register',function(Request $request, Response $response){
     $city = $request->getParam('CITY');
     $country = $request->getParam('COUNTRY');
     $street = $request->getParam('STREET');
+
     $passswordHash = hash ("sha256" , $request->getParam('PASSWORD'));
+
     $query = "insert INTO users (username,name,surname,email,password,phone,cap,city,id_group,country,street) VALUES ('".$user."','".$name."','".$surname."','".$email."','".$passswordHash."','".$phone."','".$cap."','".$city."',1,'".$country."','".$street."')";
+
     try
     {
         $db = new db();
         //connect 
         $db = $db->connect();
         $stmt = oci_parse($db, $query);
-        $r = oci_execute($stmt);
         //check errors
-        if (!$r) 
+        if (!oci_execute($stmt)) 
         {
-            $e = oci_error($stmt);
-            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+            $response->getBody()->write("one or more rows are not correct");
+            //$e = oci_error($stmt);
+            //trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
         }
         else {
             //responce data
-            echo json_encode(true);
+            $response->getBody()->write("true");
         }
-        
-        
-        
-
         //close connection
         $customers = oci_free_statement($stmt);
         oci_close($db);
